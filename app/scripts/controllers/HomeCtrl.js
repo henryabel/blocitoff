19de423204a9aa34b6c140beff591f3e5acdd801
Henry Abel <henryabel@fastmail.com>
2017-10-23 22:47:16 -0400

    Checkpoint 3: completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,13 +1,15 @@
 (function () {
     function HomeCtrl($firebaseArray) {
         vm = this;
-        console.log("home");
         var ref = firebase.database().ref();
         var taskList = $firebaseArray(ref);
         vm.$add = function (task) {
             taskList.$add(task);
         };
         //vm.$add("test3");
+        vm.fetchTasks = function () {
+            return taskList;
+        };
     }
     angular.module('blocItOff').controller('HomeCtrl', ['$firebaseArray', HomeCtrl]);
 })();
